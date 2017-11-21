3ce8ad5fcd80e48507cd80158fc2d05b5a7707f8
avoajaugochukwu <karlthegoo@hotmail.com>
2017-11-21 08:49:34 +0100

    game working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Game from './Game';
 
 class App extends Component {
+  state = {
+    gameId: 1,
+  };
+
+  resetGame = () => 
+    this.setState((prevState) => ({
+      gameId: prevState.gameId + 1,
+    }));
+
   render() {
     return (
       <Game 
+        key={this.state.gameId}
+        autoPlay={this.state.gameId > 1}
         challengeSize={6} 
         challengeRange={[2, 9]} 
         initialSeconds={10}
+        onPlayAgain={this.resetGame}
       />
     );
   }
--- src/Game.js
+++ src/Game.js
@@ -1,93 +1,142 @@
 import React, { Component } from 'react';
+var _ = require('lodash');
 
-const randomNumberBetween = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const randomNumberBetween = (min, max) =>
+Math.floor(Math.random() * (max - min + 1)) + min;
 
-class Number extends Component {
-    render() {
-        return (
-            <div className="number"
-                style={{ opacity: this.props.clickable ? 1 : 0.3 }}
-                onClick={() => console.log(this.props.id)}
-            >
-                {this.props.value}
-            </div>
-        );
-    }
+class Number extends React.PureComponent {
+handleClick = () => {
+  if (this.props.clickable) {
+    this.props.onClick(this.props.id);
+  }
+};
+
+render() {
+  return (
+    <div
+      className="number"
+      style={{ opacity: this.props.clickable ? 1 : 0.3 }}
+      onClick={this.handleClick}
+    >
+      {this.props.value}
+    </div>
+  );
+}
 }
 
-export default class Game extends Component {
-    challengeNumbers = Array
-        .from({ length: this.props.challengeSize })
-        .map(() => randomNumberBetween(...this.props.challengeRange));
+export default class Game extends React.Component {
+static bgColors = {
+  playing: '#ccc',
+  won: 'green',
+  lost: 'red',
+};
 
-    target = _.sampleSize(
-        this.challengeNumbers,
-        this.props.challengeSize - 2
-    ).reduce((acc, curr) => acc + curr, 0)
+state = {
+  gameStatus: 'new',
+  remainingSeconds: this.props.initialSeconds,
+  selectedIds: [],
+};
 
-    state = {
-        gameStatus: 'new', //new, playing, won, lost
-        remainingSeconds: this.props.initialSeconds,
-        selectedIds: [],
+challengeNumbers = Array
+  .from({ length: this.props.challengeSize })
+  .map(() => randomNumberBetween(...this.props.challengeRange));
+
+target = _.sampleSize(
+  this.challengeNumbers,
+  this.props.challengeSize - 2
+).reduce((acc, curr) => acc + curr, 0);
+
+componentDidMount() {
+  if (this.props.autoPlay) {
+    this.startGame();
+  }
+}
+
+componentWillUnmount() {
+  clearInterval(this.intervalId);
+}
+
+startGame = () => {
+  this.setState({ gameStatus: 'playing' }, () => {
+    this.intervalId = setInterval(() => {
+      this.setState((prevState) => {
+        const newRemainingSeconds = prevState.remainingSeconds - 1;
+        if (newRemainingSeconds === 0) {
+          clearInterval(this.intervalId);
+          return { gameStatus: 'lost', remainingSeconds: 0 };
+        }
+        return { remainingSeconds: newRemainingSeconds };
+      });
+    }, 1000);
+  });
+};
+
+isNumberAvailable = (numberIndex) =>
+  this.state.selectedIds.indexOf(numberIndex) === -1;
+
+selectNumber = (numberIndex) => {
+  if (this.state.gameStatus !== 'playing') {
+    return;
+  }
+  this.setState(
+    (prevState) => ({
+      selectedIds: [...prevState.selectedIds, numberIndex],
+      gameStatus: this.calcGameStatus([
+        ...prevState.selectedIds,
+        numberIndex,
+      ]),
+    }),
+    () => {
+      if (this.state.gameStatus !== 'playing') {
+        clearInterval(this.intervalId);
+      }
     }
+  );
+};
 
-    static = {
-        'playing': '#ccc',
-        'won': 'green',
-        'lost': 'red',
-    }
+calcGameStatus = (selectedIds) => {
+  const sumSelected = selectedIds.reduce(
+    (acc, curr) => acc + this.challengeNumbers[curr],
+    0
+  );
+  if (sumSelected < this.target) {
+    return 'playing';
+  }
+  return sumSelected === this.target ? 'won' : 'lost';
+};
 
-    isNumberAvailable = (numberIndex) => this.state.selectedIds.indexOf(numberIndex) === 1;
-
-    startGame = () => {
-        this.setState({ gameStatus: 'playing' }, () => {
-            this.intervalId = setInterval(() => {
-                this.setState((prevState) => {
-                    const newRemainingSeconds = prevState.remainingSeconds - 1;
-                    
-                    if (newRemainingSeconds === 0) {
-                        clearInterval(this.intervalId);
-                        return { gameStatus: 'lost', remainingSeconds: 0 };
-                    }
-                    return { remainingSeconds: newRemainingSeconds };
-                });
-            }, 1000);
-        });
-    };
-
-    // selectNumber
-
-    render() {
-        return (
-          <div className="game">
-            <div 
-                className="target"
-                style={{ backgroundColor: Game.bgcolors[gameStatus] }}
-            >
-                {this.state.gameStatus === 'new' ? '?' : this.target}
-            </div>
-            <div className="challenge-numbers">
-                {this.challengeNumbers.map((value, index) =>
-                <Number 
-                    key={index} 
-                    id={index}
-                    value={value}
-                    clickable={this.isNumberAvailable(index)}
-                />
-                )}
-            </div>
-            <div className="footer">
-                {this.state.gameStatus === 'new' ? (
-                    <button>Start</button>
-                ) : (
-                    <div className="timer-value">{this.state.remainingSeconds}</div>      
-                )}
-                {['won', 'lost'].includes(this.state.gameStatus) && (
-                    <button>Play Again</button>
-                )}
-              <button>Start</button>
-            </div>
-          </div>
-        );
-    }
+render() {
+  const { gameStatus, remainingSeconds } = this.state;
+  return (
+    <div className="game">
+      <div
+        className="target"
+        style={{ backgroundColor: Game.bgColors[gameStatus] }}
+      >
+        {gameStatus === 'new' ? '?' : this.target}
+      </div>
+      <div className="challenge-numbers">
+        {this.challengeNumbers.map((value, index) => (
+          <Number
+            key={index}
+            id={index}
+            value={gameStatus === 'new' ? '?' : value}
+            clickable={this.isNumberAvailable(index)}
+            onClick={this.selectNumber}
+          />
+        ))}
+      </div>
+      <div className="footer">
+        {gameStatus === 'new' ? (
+          <button onClick={this.startGame}>Start</button>
+        ) : (
+          <div className="timer-value">{remainingSeconds}</div>
+        )}
+        {['won', 'lost'].includes(gameStatus) && (
+          <button onClick={this.props.onPlayAgain}>Play Again</button>
+        )}
+      </div>
+    </div>
+  );
+}
 }
